b4c7ba8d6830bccb51268c6dd59adc0576c580f6
Jeff Tian <jeff.tian@outlook.com>
2021-11-26 16:23:03 +0800

    fix: integration test

--- src/test/java/example/IntegrationTest.java
+++ src/test/java/example/IntegrationTest.java
@@ -5,6 +5,7 @@ import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.Schema;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pulsar.Event;
 
@@ -14,19 +15,22 @@ import java.util.Objects;
 public class IntegrationTest {
     static final String PULSAR_URL = "pulsar://localhost:6650";
     static PulsarClient pulsarClient;
-    final String topicName = !Objects.equals(System.getenv("PULSAR_TOPIC_NAME"), "") ? System.getenv("") : "Hackathon.Producer.Green.Employees";
-    final String subscriptionName = !Objects.equals(System.getenv("PULSAR_SUBSCRIPTION_NAME"), "") ? System.getenv("PULSAR_SUBSCRIPTION_NAME") : "Hackathon.Team13.Consumer";
+    static final String topicName = System.getenv("PULSAR_TOPIC_NAME") != null && !Objects.equals(System.getenv("PULSAR_TOPIC_NAME"), "") ? System.getenv("PULSAR_TOPIC_NAME") : "Hackathon.Producer.Green.Employees";
+    static final String subscriptionName = System.getenv("PULSAR_SUBSCRIPTION_NAME") != null && !Objects.equals(System.getenv("PULSAR_SUBSCRIPTION_NAME"), "") ? System.getenv("PULSAR_SUBSCRIPTION_NAME") : "Hackathon.Team13.Consumer";
 
     static {
         try {
             pulsarClient = PulsarClient.builder().serviceUrl(PULSAR_URL).build();
+
         } catch (PulsarClientException e) {
             e.printStackTrace();
         }
     }
 
-    @Test
-    void produceEvents() throws PulsarClientException {
+    @BeforeAll
+    static void setup() throws PulsarClientException {
+        assert topicName != null;
+
         Producer<Event> producer = pulsarClient.newProducer(Schema.JSON(Event.class)).topic(topicName).create();
         producer.send(new Event(391, "Glen Carroll", "tinny", "TeamX", "added", new Date(), false));
         producer.close();
